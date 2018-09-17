6505b85f6b41c3892812f8d6ca6f2cc2938079bc
khanhas <xuankhanh963@gmail.com>
2018-09-17 18:19:44 +0700

    breaking: new RmAPI wrapper, child's parent option name

--- plugin.cs
+++ plugin.cs
@@ -30,6 +30,7 @@ namespace PowershellRM
         internal ScriptTypes scriptType = ScriptTypes.NOTVALID;
         internal string script;
         internal string output;
+        internal RmAPIWrapper apiWrapper;
 
         internal virtual void SetRmAPI() { }
 
@@ -54,7 +55,7 @@ namespace PowershellRM
                 {
                     script += command + "\n";
                     ++lineNum;
-                    command = rmAPI.ReadString(baseLine + lineNum, "");
+                    command = rmAPI.ReadString(baseLine + lineNum, "", false);
                 }
 
                 scriptType = ScriptTypes.LINE;
@@ -149,6 +150,8 @@ namespace PowershellRM
         internal ParentMeasure(API api)
         {
             rmAPI = api;
+            apiWrapper = new RmAPIWrapper(api);
+
             measureName = api.GetMeasureName().ToLowerInvariant();
             Skin = api.GetSkin();
 
@@ -230,8 +233,10 @@ namespace PowershellRM
 
         internal override void Dispose()
         {
-            CommandInfo updateFuncInfo = runspace.SessionStateProxy.InvokeCommand.GetCommand("Finalize", CommandTypes.Function);
-            if (updateFuncInfo != null)
+            CommandInfo finalizeFuncInfo = runspace.SessionStateProxy
+                .InvokeCommand.GetCommand("Finalize", CommandTypes.Function);
+
+            if (finalizeFuncInfo != null)
             {
                 using (Pipeline pipe = runspace.CreatePipeline())
                 {
@@ -239,14 +244,14 @@ namespace PowershellRM
                     pipe.Invoke();
                 }
             }
-            
+
             runspace.Dispose();
             ParentMeasures.Remove(this);
         }
 
         internal override void SetRmAPI()
         {
-            runspace.SessionStateProxy.SetVariable("RMAPI", rmAPI);
+            runspace.SessionStateProxy.SetVariable("RMAPI", apiWrapper);
             rmPSHost.Ui.RainmeterAPI = rmAPI;
         }
     }
@@ -258,7 +263,9 @@ namespace PowershellRM
         internal ChildMeasure(API api)
         {
             rmAPI = api;
-            string parentName = api.ReadString("ParentName", "").ToLowerInvariant();
+            apiWrapper = new RmAPIWrapper(api);
+
+            string parentName = api.ReadString("Parent", "").ToLowerInvariant();
             IntPtr skin = api.GetSkin();
             measureName = api.GetMeasureName();
 
@@ -275,7 +282,7 @@ namespace PowershellRM
 
             if (parent == null)
             {
-                api.Log(API.LogType.Error, "PowershellRM.dll: ParentName=" + parentName + " not valid");
+                api.Log(API.LogType.Error, "PowershellRM.dll: Parent=" + parentName + " not valid");
                 return;
             }
 
@@ -284,7 +291,7 @@ namespace PowershellRM
 
         internal override void SetRmAPI()
         {
-            runspace.SessionStateProxy.SetVariable("RMAPI", rmAPI);
+            runspace.SessionStateProxy.SetVariable("RMAPI", apiWrapper);
             parent.rmPSHost.Ui.RainmeterAPI = rmAPI;
         }
     }
@@ -298,7 +305,7 @@ namespace PowershellRM
         {
             API api = new API(rm);
 
-            string parent = api.ReadString("ParentName", "");
+            string parent = api.ReadString("Parent", "");
 
             Measure measure;
             if (string.IsNullOrEmpty(parent))
@@ -565,6 +572,120 @@ internal class RmPSHostUserInterface : PSHostUserInterface
     public override System.Security.SecureString ReadLineAsSecureString()
     {
         throw new NotImplementedException();
+    }
+}
+
+internal class RmAPIWrapper
+{
+    private API _rm;
+
+    public RmAPIWrapper(API rm)
+    {
+        _rm = rm;
+    }
+
+    public void Bang(string bangs)
+    {
+        _rm.Execute(bangs);
+    }
+
+    public double Measure(string measure)
+    {
+        string value = _rm.ReplaceVariables("[" + measure + ":]");
+        double.TryParse(value, out double result);
+        return result;
+    }
+
+    public string MeasureStr(string measure)
+    {
+        return _rm.ReplaceVariables("[" + measure + "]");
+    }
+
+    public double Variable(string var)
+    {
+        string value = _rm.ReplaceVariables("#" + var + "#");
+        double.TryParse(value, out double result);
+        return result;
+    }
+
+    public string VariableStr(string var)
+    {
+        return _rm.ReplaceVariables("#" + var + "#");
+    }
+
+    public string ReplaceVariables(string input)
+    {
+        return _rm.ReplaceVariables(input);
+    }
+
+    public double Option(string key, double defaultValue = 0.0)
+    {
+        return _rm.ReadDouble(key, defaultValue);
+    }
+
+    public int Option(string key, int defaultValue = 0)
+    {
+        return _rm.ReadInt(key, defaultValue);
+    }
+
+    public string Option(string key, string defaultValue = "")
+    {
+        return _rm.ReadString(key, defaultValue);
+    }
+
+    public string OptionPath(string key, string defaultValue = "")
+    {
+        return _rm.ReadPath(key, defaultValue);
+    }
+
+    public void Log(string message)
+    {
+        _rm.Log(API.LogType.Notice, message);
+    }
+
+    public void LogError(string message)
+    {
+        _rm.Log(API.LogType.Error, message);
+    }
+
+    public void LogWarning(string message)
+    {
+        _rm.Log(API.LogType.Warning, message);
+    }
+
+    public void LogDebug(string message)
+    {
+        _rm.Log(API.LogType.Debug, message);
+    }
+
+    public void Log(string format, params Object[] args)
+    {
+        _rm.LogF(API.LogType.Notice, format, args);
+    }
+
+    public void LogError(string format, params Object[] args)
+    {
+        _rm.LogF(API.LogType.Error, format, args);
+    }
+
+    public void LogWarning(string format, params Object[] args)
+    {
+        _rm.LogF(API.LogType.Warning, format, args);
+    }
+
+    public void LogDebug(string format, params Object[] args)
+    {
+        _rm.LogF(API.LogType.Debug, format, args);
+    }
+
+    public string GetSkinName()
+    {
+        return _rm.GetSkinName();
+    }
+
+    public IntPtr GetSkinHandle()
+    {
+        return _rm.GetSkinWindow();
     }
 }
 }
